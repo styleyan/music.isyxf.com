a87fb4f6f3c09f85e67e1204adff8455a4f556ae
yanxiaofei <forget_love2161@126.com>
2017-09-06 00:25:33 +0800

    fix: 增加排行榜详情页面数据获取与应用

--- src/views/rank/Index.vue
+++ src/views/rank/Index.vue
@@ -1,10 +1,118 @@
 <template>
-  <div>
-    排行榜
+  <div class="rank" ref="rank">
+    <scroll :data="topList" ref="toplist" class="toplist">
+      <ul>
+        <li class="item"
+          v-for="(item, key) in topList"
+          :key="key"
+          >
+          <div class="icon">
+            <img width="100" height="100" v-lazy="item.picUrl" />
+          </div>
+          <ul class="songlist">
+            <li class="song"
+              v-for="(song, key) in item.songList"
+              :key="key"
+              >
+              <span>{{key + 1}}</span>
+              <span>{{song.songname}}-{{song.singgername}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div class="loading-container" v-show="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import {getTopList} from '@api/rank'
+import {ERR_OK} from '@api/config'
+import Scroll from '@components/scroll/Scroll.vue'
+import Loading from '@components/loading/Loading.vue'
+import {playlistMixin} from '@mixin'
+
 export default {
+  name: 'rank',
+  mixins: [playlistMixin],
+  components: {
+    Scroll,
+    Loading,
+  },
+  created() {
+    this._getTopList()
+  },
+  data() {
+    return {
+      topList: [],
+    }
+  },
+  methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length ? '60px' : ''
+      this.$refs.rank.style.bottom = bottom
+      this.$refs.toplist.refresh()
+    },
+    _getTopList() {
+      getTopList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.topList = res.data.topList
+        }
+      })
+    },
+  },
 }
 </script>
+
+<style scoped lang="stylus">
+  @import "~@stylus/variable"
+  @import "~@stylus/mixin"
+
+.rank
+  position: fixed
+  width: 100%
+  top: 88px
+  bottom: 0
+
+.toplist
+  height: 100%
+  overflow: hidden
+
+.item
+  display: flex
+  margin: 0 20px
+  padding-top: 20px
+  height: 100px
+  &:last-child
+    padding-bottom: 20px
+
+.icon
+  flex: 0 0 100px
+  width: 100px
+  height: 100px
+
+.songlist
+  flex: 1
+  display: flex
+  flex-direction: column
+  justify-content: center
+  padding: 0 20px
+  height: 100px
+  overflow: hidden
+  background: $color-highlight-background
+  color: $color-text-d
+  font-size: $font-size-small
+
+.song
+  no-wrap()
+  line-height: 26px
+
+.loading-container
+  position: absolute
+  width: 100%
+  top: 50%
+  transform: translateY(-50%)
+</style>
