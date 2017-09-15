0020ab1425b5a74f6be8ec03f73adcaa1cc37cce
yanxiaofei <forget_love2161@126.com>
2017-09-16 01:18:05 +0800

    fix: 懒加载每个页面的js文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from '@views/recommend/Index'
-import RecommendDetail from '@views/recommend/Detail'
-import Rank from '@views/rank/Index'
-import RankDetail from '@views/rank/Detail'
-import Search from '@views/search/Index'
-import Singer from '@views/singer/Index'
-import SingerDetail from '@views/singer/Detail'
-import User from '@views/user/Index'
-
 Vue.use(Router)
+
+const Recommend = (resolve) => {
+  import('@views/recommend/Index').then((module) => {
+    resolve(module)
+  })
+}
+
+const RecommendDetail = (resolve) => {
+  import('@views/recommend/Detail').then((module) => {
+    resolve(module)
+  })
+}
+
+const Rank = (resolve) => {
+  import('@views/rank/Index').then((module) => {
+    resolve(module)
+  })
+}
+
+const RankDetail = (resolve) => {
+  import('@views/rank/Detail').then((module) => {
+    resolve(module)
+  })
+}
+
+const Search = (resolve) => {
+  import('@views/search/Index').then((module) => {
+    resolve(module)
+  })
+}
+
+const Singer = (resolve) => {
+  import('@views/singer/Index').then((module) => {
+    resolve(module)
+  })
+}
+
+const SingerDetail = (resolve) => {
+  import('@views/singer/Detail').then((module) => {
+    resolve(module)
+  })
+}
+
+const User = (resolve) => {
+  import('@views/user/Index').then((module) => {
+    resolve(module)
+  })
+}
 
 export default new Router({
   routes: [
